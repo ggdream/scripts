94329e7de08f4c89d6a7d8e447c9fe33bda2ac1d
魔咔啦咔 <gdream@yeah.net>
2020-10-06 23:26:19 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -3,23 +3,21 @@ version=3.8.5
 location=/usr/local/python
 
 
-sudo su
-
-apt install -y zlib1g-dev gcc make build-essential libssl-dev \
+sudo apt install -y zlib1g-dev gcc make build-essential libssl-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev openssl
 
-wget https://npm.taobao.org/mirrors/python/$version/Python-$version.tgz
+sudo wget https://npm.taobao.org/mirrors/python/$version/Python-$version.tgz
 tar -zxvf Python-$version.tgz
 cd Python-$version/
 
 ./configure --prefix=$location
-make -j4 && make install
+sudo make -j4 && sudo make install
 
-rm -rf /usr/bin/pip3 /usr/bin/python3
-ln -s $location/bin/pip3 /usr/bin/pip3
-ln -s $location/bin/python3 /usr/bin/python3
+sudo rm -rf /usr/bin/pip3 /usr/bin/python3
+sudo ln -s $location/bin/pip3 /usr/bin/pip3
+sudo ln -s $location/bin/python3 /usr/bin/python3
 
-cp /usr/lib/python3/dist-packages/lsb_release.py $location/lib/python3.8/
-pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple
-python3 -m pip install --upgrade pip
+sudo cp /usr/lib/python3/dist-packages/lsb_release.py $location/lib/python3.8/
+sudo pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple
+sudo python3 -m pip install --upgrade pip
