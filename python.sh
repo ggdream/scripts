e250803948d3dea05c375600fe310b4df30c7b39
魔咔啦咔 <gdream@yeah.net>
2020-10-07 14:05:48 +0800

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 # ubuntu 18.04
 
-version=3.8.5
-location=/usr/local/python
+default_version=3.8.5
+default_location=/usr/local/python
+target=/etc/profile
 
+
+if [ -n "$2" ] ;then
+    version=$1
+    location=$2
+elif [ -n "$1" ] ;then
+    version=$1
+    location=$default_location
+else
+    echo "\e[31;40;1mYou have not entered any parameters, v$default_version will be selected by default.\e[0m"
+    version=$default_version
+    location=$default_location
+fi
 
 sudo apt install -y zlib1g-dev gcc make build-essential libssl-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
@@ -19,6 +32,7 @@ sudo make -j4 && sudo make install
 # sudo rm -rf /usr/bin/pip3 /usr/bin/python3
 sudo ln -s $location/bin/pip3 /usr/bin/pip3.8
 sudo ln -s $location/bin/python3 /usr/bin/python3.8
+echo "export PATH=$PATH:$location/bin" >> $target
 
 # sudo cp /usr/lib/python3/dist-packages/lsb_release.py $location/lib/python3.8/
 sudo pip3.8 config set global.index-url https://mirrors.aliyun.com/pypi/simple
