ee69aa2e50514d6d6d8410f309d2bec36ef29fc8
ggdream <gdream@yeah.net>
2020-12-24 21:09:04 +0800

    version: 1.27.4

--- docker.sh
+++ docker.sh
@@ -27,6 +27,7 @@ sudo systemctl restart docker
 # docker-compose
 # sudo apt install -y python3-pip
 # sudo pip3 install -i https://mirrors.aliyun.com/pypi/simple docker-compose
+version=1.27.4
 curl -L "https://github.91chifun.workers.dev//https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
