d35bb1222dfef907913ddf29a77a692e8b3867bd
ggdream <gdream@yeah.net>
2020-12-24 21:07:48 +0800

    docker-compose: pip -> gh-dist

--- docker.sh
+++ docker.sh
@@ -25,7 +25,10 @@ sudo systemctl restart docker
 
 
 # docker-compose
-sudo apt install -y python3-pip
-sudo pip3 install -i https://mirrors.aliyun.com/pypi/simple docker-compose
+# sudo apt install -y python3-pip
+# sudo pip3 install -i https://mirrors.aliyun.com/pypi/simple docker-compose
+curl -L "https://github.91chifun.workers.dev//https://github.com/docker/compose/releases/download/$version/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 
 docker -v && docker-compose -v
