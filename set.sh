1a97e022f1315f11c9ceaaadd961d13e882a9134
ggdream <gdream@yeah.net>
2020-12-24 22:07:40 +0800

    delete start and end logs

--- set.sh
+++ set.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 
-echo "\e[31;40;1mStarting...\e[0m"
-sleep 1s
-
 for i in $@ ;do
     wget https://cdn.jsdelivr.net/gh/ggdream/scripts@main/$i.sh && bash $i.sh
 done
 
-echo "\e[31;40;1mDone.\e[0m"
 rm -rf *
