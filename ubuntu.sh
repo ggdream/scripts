d46da7f210655ffe412e42d9380f463623ee1676
ggdream <gdream@yeah.net>
2020-12-25 11:14:05 +0800

    Revert "focal"
    This reverts commit af57836d06d7a353b308bd3088fa734851696f27.

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,21 +2,18 @@
 
 
 sudo mv /etc/apt/sources.list /etc/apt/sourses.list.backup
-
-CODE=$(lsb_release -c | awk '{ print $(NF) }')
-sudo cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/ubuntu/ $CODE main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ $CODE-security main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ $CODE-updates main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ $CODE-proposed main restricted universe multiverse
-deb http://mirrors.aliyun.com/ubuntu/ $CODE-backports main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $CODE main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $CODE-security main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $CODE-updates main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $CODE-proposed main restricted universe multiverse
-deb-src http://mirrors.aliyun.com/ubuntu/ $CODE-backports main restricted universe multiverse
+sudo tee /etc/apt/sources.list <<-'EOF'
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
 EOF
-
 
 # sudo sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/g' /etc/apt/sources.list
 apt update && sudo apt upgrade -y
