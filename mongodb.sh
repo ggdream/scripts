142f2e1702360bf1715a7a289cf8ef9f0a55dafc
ggdream <gdream@yeah.net>
2020-12-25 11:14:39 +0800

    focal

--- mongodb.sh
+++ mongodb.sh
@@ -12,7 +12,7 @@ else
 fi
 
 wget -qO - https://www.mongodb.org/static/pgp/server-$version.asc | sudo apt-key add -
-echo "deb https://mirrors.tuna.tsinghua.edu.cn/mongodb/apt/ubuntu bionic/mongodb-org/$version multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$version.list # 18.04
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/mongodb/apt/ubuntu focal/mongodb-org/$version multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$version.list # 18.04
 # echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/$version multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-$version.list  # 20.04
 
 
